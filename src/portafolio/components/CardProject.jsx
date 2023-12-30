14c159b54775b94d862c4f988efe5e68c8dab8bb
Fernando lopez <ferchholop18@hotmail.com>
2023-12-30 09:11:57 -0500

    index.html atualizado

--- src/portafolio/components/CardProject.jsx
+++ src/portafolio/components/CardProject.jsx
@@ -4,7 +4,7 @@
 export const CardProject = ({ titulo, url, descripcion, githubUrl, link_project }) => {
 
   return (
-    <div className="col-sm-6 col-md-4 m-0">
+    <div className="col-sm-6 col-md-4 mb-2">
       <div className="card ">
         <img className="card-img-top img-card" src={url} />
         <div className="card-body">
